c2610d867106fe2a0f50e6466c416b3377596083
Vitaliy Mashkov <vitaliy.mashkov@developex.com>
2023-08-02 19:51:27 +0300

    Base functionality

--- src/js/app.js
+++ src/js/app.js
@@ -4,31 +4,79 @@ import ReactDOM from 'react-dom';
 
 // Required for work on iOS 9b
 import 'babel-polyfill';
+import i18next from './i18n';
 
-import HelloWorldApp from '../../examples/hello-world/app';
-import LoyaltyApp from '../../examples/loyalty/app';
-import DeviceHandshakeApp from '../../examples/device-handshake/app';
-import TextPrintApp from '../../examples/text-print/app';
-import PmsApp from '../../examples/pms/App';
+class PosterTaxSubtotal extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            emoji: '',
+            message: '',
+        };
+        i18next.changeLanguage(Poster.settings.lang);
+        // Показываем кнопки приложения в окне настроек и заказа
+        Poster.interface.showApplicationIconAt({
+            payment: i18next.t('Tax subtotal'),
+        });
 
-class ExampleApp extends React.Component {
+        // Подписываемся на клик по кнопке
+        Poster.on('applicationIconClicked', () => {
+            const self = this;
+            Poster.orders.getActive()
+                .then(async (order) => {
+                    const products = Object.values(order.order.products);
+                    Poster.products.get(products.map(i => i.id))
+                        .then((ps) => {
+                            const taxes = {};
+                            let total = 0;
+                            for(const p in ps) {
+                                const initialValue = 0;
+                                const summ = products.filter(i => i.id === ps[p].id).map(i => i.count * i.price).reduce(
+                                    (accumulator, currentValue) => accumulator + currentValue,
+                                    initialValue
+                                );
+                                ps[p].summ = summ;
+                                if (!Object.hasOwn(ps[p].taxId)) {
+                                    taxes[ps[p].taxId] = {
+                                        taxId: ps[p].taxId,
+                                        title: ps[p].taxName,
+                                        summ: 0
+                                    }
+                                }
+                                taxes[ps[p].taxId].summ += summ;
+                                total += summ;
+                            }
+                            taxes[99999] = {
+                                taxId: 99999,
+                                title: `<strong>${i18next.t('Total')}</strong>`,
+                                summ: total
+                            };
+                            const msgItems = Object.values(taxes)
+                                .map(i => `<li>${i.title}: <strong>${i.summ} ${Poster.settings.currencySymbol}</strong></li>`);
+                            self.setState({
+                                emoji: '💵',
+                                message: `<ul>${msgItems.join('')}</ul>` });
+                        });
+                    // Показываем интерфейс
+                    Poster.interface.popup({ width: 500, height: 400, title: i18next.t('Tax subtotal') });
+                });
+        });
+    }
+
     render() {
-        // Чтобы отобразить нужный пример просто закомментируйте не нужныйе компоненты
+        const { emoji, message } = this.state;
 
-        return <HelloWorldApp />;
-
-        return <LoyaltyApp />;
-
-        return <DeviceHandshakeApp />;
-
-        return <TextPrintApp />;
-
-        return <PmsApp />;
+        return (
+            <div className="tax-subtotal">
+                <h1>{emoji}</h1>
+                <p dangerouslySetInnerHTML={{ __html: message }}></p>
+            </div>
+        );
     }
 }
 
 ReactDOM.render(
-    <ExampleApp />,
+    <PosterTaxSubtotal />,
     document.getElementById('app-container'),
 );
